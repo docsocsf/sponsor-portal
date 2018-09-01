8bdc2b4a415502c7c7c903eb88f408b844837a74
Jack Pordi <jackel119@gmail.com>
2018-09-01 21:38:08 +0100

    Now ssh's into vm and updates it too

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-//setup express app
+// setup express app
 const setup = require('./src/setup.js')
 const app = setup.app
 
-//setup mongoDB database
+// setup mongoDB database
 const db = require('./src/db.js')
 
-//==========================LOGIN PAGE======================
+//= =========================LOGIN PAGE======================
 const login = require('./src/login.js')
 login.setup(app, db)
 
-//===========================MEMBER=========================
+//= ==========================MEMBER=========================
 
 const member = require('./src/member.js')
 member.setup(app, db)
 
-//===========================SPONSOR========================
+//= ==========================SPONSOR========================
 
 const sponsor = require('./src/sponsor.js')
 sponsor.setup(app, db)
 
-//===========================PORTAL=========================
-  
+//= ==========================PORTAL=========================
+
 const portal = require('./src/portal.js')
 portal.setup(app, db)
 
-//============================OTHER=========================
+//= ===========================OTHER=========================
 
-//LOGOUT
-app.post('/logout', (req,res) => {
-  req.session.destroy() 
-  res.redirect('/') 
-}) 
+// LOGOUT
+app.post('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
 
-app.get('*', function(req, res){
-  res.redirect('/login') 
-});
+app.get('*', function (req, res) {
+  res.redirect('/login')
+})
 
-
-//SERVER LISTEN
-app.listen(app.get('port'), function(){
-  console.log('Server listening on port ' + app.get('port')) 
-})                             
+// SERVER LISTEN
+app.listen(app.get('port'), function () {
+  console.log('Server listening on port ' + app.get('port'))
+})
