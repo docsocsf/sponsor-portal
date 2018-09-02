ed6a5641d6817c6fbb8dcd1155886fa9611398a4
gs2617 <gs2617@ic.ac.uk>
2018-09-02 09:25:48 +0100

    added bespoke

--- src/portal.js
+++ src/portal.js
@@ -47,8 +47,8 @@ exports.setup = (app, db) => {
       info: {
         name: req.body.name,
         rank: req.body.rank,
-        picture: req.body.user+ '_logo.png'
-        // bespoke
+        picture: req.body.user+ '_logo.png',
+        bespoke: (req.body.bespoke === 'true')
       },
       news: [],
       positions: []
@@ -68,13 +68,62 @@ exports.setup = (app, db) => {
     }) 
   }) 
 
-  //Remove db.Sponsor
+
+  //Edit sponsor
+  app.post('/edit-sponsor/:username', (req,res) => {
+    db.Sponsor.find({username: req.params.username} , (err, sponsor) => {
+      if (err) return 
+      
+      sponsor[0].password = req.body['s.password']
+      if(req.body['s.info.name'])
+        sponsor[0].info.name = req.body['s.info.name']
+      if(req.body['s.info.email'])
+        sponsor[0].info.email = req.body['s.info.email']
+      if(req.body['s.info.rank'])
+        sponsor[0].info.rank = req.body['s.info.rank']
+      if(req.body['s.info.bespoke'])
+        sponsor[0].info.bespoke = (req.body['s.info.bespoke'] === 'true')
+      if(req.body['s.info.description'])
+        sponsor[0].info.description = req.body['s.info.description']
+      if(req.body['s.info.picture'])
+        sponsor[0].info.picture = req.body['s.info.picture']
+      if(req.body['s.info.link'])
+        sponsor[0].info.link = req.body['s.info.link']
+      for (let j = 0; j < sponsor[0].positions.length; j++) {
+        for (let i = 0; i < sponsor[0].positions.length; i++) {
+          if(sponsor[0].positions[j].name === req.body['p.id.'+i]){
+            if(req.body['p.name.'+i])
+              sponsor[0].positions[j].name = req.body['p.name.'+i]
+            if(req.body['p.description.'+i])
+              sponsor[0].positions[j].description = req.body['p.description.'+i]
+            if(req.body['p.requirements.'+i])
+              sponsor[0].positions[j].requirements = req.body['p.requirements.'+i]
+            if(req.body['p.link.'+i])
+              sponsor[0].positions[j].link = req.body['p.link.'+i]
+          }
+        }
+      }
+
+      console.log(sponsor[0].info.bespoke)
+
+      sponsor[0].save((err, user) => {
+        if (err) return 
+        res.redirect('/portal')
+      }) 
+
+    }) 
+  })
+
+  //Remove Sponsor
   app.post('/remove-sponsor/:user', (req,res) => {
     db.Sponsor.remove({username: req.params.user} , (err) => {
       if (err) {
         return  
       } else {
-        res.redirect('/portal') 
+        if(fs.existsSync('../sponsors/' + req.params.user)){
+          fs.removeSync('../sponsors/' + req.params.user)
+          res.redirect('/portal')  
+        }
       }
     }) 
   }) 
